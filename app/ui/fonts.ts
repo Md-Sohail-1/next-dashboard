0de32dc129274462facf3ff7adc97ab8672340c6
Md Sohail <sohil931376@gmail.com>
2026-02-04 12:07:21 +0530

    chapter 5 completed

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Raleway } from "next/font/google"
+import { Lusitana } from "next/font/google"
+
+export const raleway = Raleway({subsets: ["latin"]})
+export const lusitana = Lusitana({weight: ["400", "700"]})
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
+  swcMinify: false,
+  experimental: {
+    forceSwcTransforms: false
+  }
+}
 
 export default nextConfig;
