ef86be6a5b3c5f9d01928092edbf38dfeff46b11
Md Sohail <sohil931376@gmail.com>
2026-02-05 16:34:53 +0530

    solve deployment error :)

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,4 +2,4 @@ import { Raleway } from "next/font/google"
 import { Lusitana } from "next/font/google"
 
 export const raleway = Raleway({subsets: ["latin"]})
-export const lusitana = Lusitana({weight: ["400", "700"]})
+export const lusitana = Lusitana({subsets: ["latin"], weight: ["400", "700"]})
